d5eb20e198d9c47da89f6390bd59ad0a8954c0e5
brahim el alami <bra.el.alami@gmail.com>
2023-12-08 18:22:19 -0800

    add

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
-# setup server for deployment web_static
-apt-get update -y
-apt-get upgrade -y
-apt-get install nginx -y
-mkdir -p /data/web_static
-mkdir -p /data/web_static/releases
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/releases/test
-echo "test deploying web_static" > /data/web_static/releases/test/index.html
-ln -fs /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/^\tserver_name/ a\\tlocation /hbnb_static \{\n\t\talias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
-service nginx restart
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
